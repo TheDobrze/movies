09206120b8b5f00b96c37b84e03b694b446c68d7
TheDobrze <Niecki19@gmail.com>
2022-12-11 00:49:55 +0100

    Initial commit

---
+++ movies/src/test/java/md/nowekolory/filmy/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package md.nowekolory.filmy.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
